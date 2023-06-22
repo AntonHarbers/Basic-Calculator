1ee121d88d460073b41614feb6389a7c00772d46
Anton Harbers <Anton.Harbers23@gmail.com>
2023-06-22 15:16:11 +0400

    Feat: GUI

---
+++ script.js
@@ -0,0 +1,33 @@
+const add = (a,b) => {
+    return a+b;
+}
+
+const subtract = (a,b) => {
+    return a-b;
+}
+
+const multiply = (a,b) => {
+    return a*b;
+}
+
+const divide = (a,b) => {
+    return a/b;
+}
+
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+const operate = (a,b,operator) => {
+    switch(operator){
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            return divide(a,b);
+    }
+}
+
